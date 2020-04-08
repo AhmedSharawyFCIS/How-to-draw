b3a510b2890b835cbb77bf7ca5cf3e1e0963f870
Ahmed Sharawy <Ahmed.Sharawy.FCIS@gmail.com>
2020-04-08 18:38:14 +0200

    Updates

---
+++ precache-manifest.07c8e51a0f9a0bcf3346fd82c84f00bf.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2269e66789f0030d5749fd5b276d2d3",
+    "url": "/How-to-draw.git/index.html"
+  },
+  {
+    "revision": "9490bc08320906a46a3a",
+    "url": "/How-to-draw.git/static/css/main.1eae6f56.chunk.css"
+  },
+  {
+    "revision": "b1751de3c55a513c8c56",
+    "url": "/How-to-draw.git/static/js/2.62113b78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/How-to-draw.git/static/js/2.62113b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9490bc08320906a46a3a",
+    "url": "/How-to-draw.git/static/js/main.ac987fb0.chunk.js"
+  },
+  {
+    "revision": "fbbc14ca674aedbf0cc9",
+    "url": "/How-to-draw.git/static/js/runtime-main.6fedb0ba.js"
+  },
+  {
+    "revision": "b691b12541f776c6e3251637cfa601b1",
+    "url": "/How-to-draw.git/static/media/animal1.b691b125.jpeg"
+  },
+  {
+    "revision": "bcc628e29670da7cb5353775f138cffc",
+    "url": "/How-to-draw.git/static/media/animal2.bcc628e2.jpeg"
+  },
+  {
+    "revision": "dfb993d6675db47447d4a58e8b70ff28",
+    "url": "/How-to-draw.git/static/media/car1.dfb993d6.jpeg"
+  },
+  {
+    "revision": "14549860e210aee8acca4652f5bb7444",
+    "url": "/How-to-draw.git/static/media/car2.14549860.jpeg"
+  },
+  {
+    "revision": "0a6a8623ab615438b9817a572d737acf",
+    "url": "/How-to-draw.git/static/media/car3.0a6a8623.jpeg"
+  },
+  {
+    "revision": "81b8a531b307130fc703c41c544fe6ae",
+    "url": "/How-to-draw.git/static/media/car4.81b8a531.jpeg"
+  },
+  {
+    "revision": "586f28829e07b8e8fb0460449f0c6250",
+    "url": "/How-to-draw.git/static/media/car5.586f2882.jpeg"
+  },
+  {
+    "revision": "571712849cfcc4267fb78d5e13fa205f",
+    "url": "/How-to-draw.git/static/media/car6.57171284.jpeg"
+  },
+  {
+    "revision": "83a8e4de127bb8a0c58a14b46d2f7211",
+    "url": "/How-to-draw.git/static/media/cow.83a8e4de.jpeg"
+  },
+  {
+    "revision": "f5f528dfd68c10b0121fb67f97411987",
+    "url": "/How-to-draw.git/static/media/flower1.f5f528df.jpeg"
+  },
+  {
+    "revision": "3284ecec05a9c0d17c952a5f41f006d6",
+    "url": "/How-to-draw.git/static/media/flower2.3284ecec.jpeg"
+  },
+  {
+    "revision": "a68b579661282cc8e9d9c6dae0dbeb93",
+    "url": "/How-to-draw.git/static/media/flower3.a68b5796.jpeg"
+  },
+  {
+    "revision": "eace202806249b7f2b03aabec7231929",
+    "url": "/How-to-draw.git/static/media/flower4.eace2028.jpeg"
+  },
+  {
+    "revision": "90ebea839c589cb739aacfeb06ce1502",
+    "url": "/How-to-draw.git/static/media/flower5.90ebea83.jpeg"
+  },
+  {
+    "revision": "4230d93a442672b27c4f2b5ed3bec584",
+    "url": "/How-to-draw.git/static/media/food1.4230d93a.jpeg"
+  },
+  {
+    "revision": "1b55fcc1e9177887d32efb3ece1922b2",
+    "url": "/How-to-draw.git/static/media/food2.1b55fcc1.jpeg"
+  },
+  {
+    "revision": "883257057d52e9fd3b79ef81f76bd96b",
+    "url": "/How-to-draw.git/static/media/food3.88325705.jpeg"
+  },
+  {
+    "revision": "cd2f625c0192c0ca17b10c44f77a0bf9",
+    "url": "/How-to-draw.git/static/media/oswald the moose.cd2f625c.jpeg"
+  },
+  {
+    "revision": "3b3c4c06a171d28e6766e7522e9dcc97",
+    "url": "/How-to-draw.git/static/media/pizza.3b3c4c06.jpeg"
+  },
+  {
+    "revision": "62c5d163904cc0fc052b71ea282a1601",
+    "url": "/How-to-draw.git/static/media/tiger.62c5d163.jpeg"
+  }
+]);
